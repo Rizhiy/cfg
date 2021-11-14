e7eed25e80c4707d5cfb5217f153252dc0f49383
Anton Vasilev <a.vasilev@neurotrade.ru>
2021-11-14 11:51:55 +0300

    feat(transforms): load_from_key_value

--- ntc/transforms.py
+++ ntc/transforms.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Union
+from typing import Any, Dict, Union
 
 import yaml
 
@@ -15,5 +15,29 @@ def load_from_file(filepath: Union[str, Path]):
             return
         with filepath.open() as f:
             cfg.update(yaml.load(f, Loader=yaml.Loader))
+
+    return _merge
+
+
+def _flat_to_structured(kv: Dict[str, Any], sep: str = ".") -> Dict[str, Any]:
+    """
+    >>> _flat_to_structured({"a.b.c": 1, "a.b2": 2})
+    {"a": {"b": {"c": 1}, "b2": 2}}
+    """
+    structured = {}
+    for key, value in kv.items():
+        key_pieces = key.split(sep)
+        here = structured
+        for piece in key_pieces[:-1]:
+            here = here.setdefault(piece, {})
+        here[key_pieces[-1]] = value
+    return structured
+
+
+def load_from_key_value(kv: Dict[str, str]):
+    structured = _flat_to_structured(kv)
+
+    def _merge(cfg: CN) -> None:
+        cfg.update(structured)
 
     return _merge
--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,10 +1,27 @@
 from pathlib import Path
 
 from ntc import CN
+from ntc.transforms import load_from_key_value
 
 DATA_DIR = Path(__file__).parent / "data" / "transforms"
 
 
 def test_load_from_file():
     cfg = CN.load(DATA_DIR / "load_from_file_changes.py")
-    assert cfg.DICT.FOO == "bar"
+    assert cfg.DICT.FOO == "foo_value_from_yaml_file"
+    assert cfg.DICT.FOO2 == "foo2_value_from_changes"
+
+
+def test_load_from_key_value():
+    from .data.base_cfg import cfg as cfg_base
+
+    cfg = cfg_base.inherit()
+    flat_data = {
+        "DICT.FOO": "foo_value_from_flat_data",
+        "STR": "str_value_from_flat_data",
+    }
+    cfg.add_transform(load_from_key_value(flat_data))
+    cfg.transform()
+    assert cfg.DICT.FOO == "foo_value_from_flat_data"
+    assert cfg.DICT.FOO2 == "foo2_value_default"
+    assert cfg.STR == "str_value_from_flat_data"
