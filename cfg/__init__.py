b4385e6c083727a3c6d0ba05dca861a6555dccd8
Artem Vasenin <avasenin@amazon.co.uk>
2023-12-18 01:55:16 +0000

    chore(pyproject.toml): Define ruff rules

--- cfg/__init__.py
+++ cfg/__init__.py
@@ -1,10 +1,8 @@
 """Python Configuration System"""
 from __future__ import annotations
 
-from .errors import *
 from .helpers import get_output_dir
-from .interfaces import *
-from .leaf import *
-from .node import *
+from .leaf import CL
+from .node import CN
 
 __version__ = "3.1.2"
